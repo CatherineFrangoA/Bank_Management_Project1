ebbda7f6d94ea798e077c0df5c56270ce84ecd9e
CatherineFrangoA <catherinefrango10@gmail.com>
2026-08-05 19:54:45 +0530

    Commit 3 - Added Withdraw Feature

--- bank_mangement_sys.py
+++ bank_mangement_sys.py
@@ -1,29 +1,23 @@
 accounts = {}
 next_id = 1
-
 while True:
     print("\n===== BANK MANAGER SYSTEM =====")
     print("1. Create Account")
     print("2. Show All Accounts")
     print("3. Deposit")
-    print("4. Exit")
-
+    print("4.withdraw")
+    print("5. Exit")
     choice = input("Enter your choice: ")
-
     if choice == "1":
         name = input("Enter Customer Name: ")
         balance = float(input("Enter Initial Balance: "))
-
         accounts[next_id] = {
             "name": name,
             "balance": balance
         }
-
         print("Account Created Successfully")
         print("Account Number:", next_id)
-
         next_id = next_id + 1
-
     elif choice == "2":
         if len(accounts) == 0:
             print("No Accounts Found")
@@ -36,17 +30,27 @@ while True:
                 print()
     elif choice == "3":
         acc_no = int(input("Enter Account Number: "))
-
         if acc_no in accounts:
             amount = float(input("Enter Deposit Amount: "))
             accounts[acc_no]["balance"] = accounts[acc_no]["balance"] + amount
-
             print("Amount Deposited Successfully")
             print("Updated Balance:", accounts[acc_no]["balance"])
-
         else:
             print("Account Not Found")
-
     elif choice == "4":
+        acc_no = int(input("Enter Account Number: "))
+        if acc_no in accounts:
+            amount = float(input("Enter Withdraw Amount: "))
+            if amount <= accounts[acc_no]["balance"]:
+                accounts[acc_no]["balance"] = accounts[acc_no]["balance"] - amount
+                print("Amount Withdrawn Successfully")
+                print("Remaining Balance:", accounts[acc_no]["balance"])
+            else:
+                print("Insufficient Balance")
+        else:
+            print("Account Not Found")
+    elif choice == "5":
         print("Thank You")
         break
+    else:
+        print("Invalid Choice")
